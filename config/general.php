09b57ffda1db954907fcddb64f86ea31fdb4cec0
Mike Pepper <mdcpepper@gmail.com>
2019-01-21 03:24:34 +0000

    Update general config for 3.1.x

--- config/general.php
+++ config/general.php
@@ -11,9 +11,6 @@
 return [
     // Global settings
     '*' => [
-        // Default Week Start Day (0 = Sunday, 1 = Monday...)
-        'defaultWeekStartDay' => 0,
-
         // Enable CSRF Protection (recommended)
         'enableCsrfProtection' => true,
 
@@ -25,26 +22,27 @@ return [
 
         // The secure key Craft will use for hashing and encrypting data
         'securityKey' => getenv('SECURITY_KEY'),
+
+        // Whether to save the project config out to config/project.yaml
+        // (see https://docs.craftcms.com/v3/project-config.html)
+        'useProjectConfigFile' => true,
     ],
 
     // Dev environment settings
     'dev' => [
-        // Base site URL
-        'siteUrl' => null,
-
-        // Dev Mode (see https://craftcms.com/support/dev-mode)
+        // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
         'devMode' => true,
     ],
 
     // Staging environment settings
     'staging' => [
-        // Base site URL
-        'siteUrl' => null,
+        // Prevent administrative changes from being made on staging
+        'allowAdminChanges' => false,
     ],
 
     // Production environment settings
     'production' => [
-        // Base site URL
-        'siteUrl' => null,
+        // Prevent administrative changes from being made on production
+        'allowAdminChanges' => false,
     ],
 ];
